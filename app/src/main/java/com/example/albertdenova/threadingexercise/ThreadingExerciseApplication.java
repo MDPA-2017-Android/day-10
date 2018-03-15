70fb0b28fe814a6d78a3d973ea5d74860bade910
albert.denova <albert.denova@king.com>
2018-03-15 18:21:59 +0100

    Setup basic local notification

--- app/src/main/java/com/example/albertdenova/threadingexercise/ThreadingExerciseApplication.java
+++ app/src/main/java/com/example/albertdenova/threadingexercise/ThreadingExerciseApplication.java
@@ -1,7 +1,11 @@
 package com.example.albertdenova.threadingexercise;
 
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.arch.persistence.room.Room;
+import android.os.Build;
+import android.support.v4.app.NotificationManagerCompat;
 
 import com.example.albertdenova.threadingexercise.database.ThreadingDatabase;
 
@@ -11,6 +15,8 @@ import toothpick.config.Module;
 
 public class ThreadingExerciseApplication extends Application {
 
+    public static final String NOTIFICATION_CHANNEL_ID = "ThreadingExerciseChannel";
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -18,11 +24,32 @@ public class ThreadingExerciseApplication extends Application {
         Scope scope = Toothpick.openScopes(this);
         scope.installModules(new Module() {{
             bind(ThreadingDatabase.class).toInstance(createDatabaseInstance());
+            bind(NotificationManager.class).toInstance((NotificationManager) getSystemService(NOTIFICATION_SERVICE));
         }});
+
+        CreateNotificationChannel();
     }
 
     private ThreadingDatabase createDatabaseInstance() {
         return Room.databaseBuilder(getApplicationContext(),
                 ThreadingDatabase.class, "database-name").build();
+    }
+
+    private void CreateNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create the NotificationChannel, but only on API 26+ because
+            // the NotificationChannel class is new and not in the support library
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system
+            NotificationManager notificationManager = (NotificationManager) getSystemService(
+                    NOTIFICATION_SERVICE);
+            assert notificationManager != null;
+
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
--- app/src/main/java/com/example/albertdenova/threadingexercise/view/model/ThreadCounterViewModel.java
+++ app/src/main/java/com/example/albertdenova/threadingexercise/view/model/ThreadCounterViewModel.java
@@ -1,22 +1,32 @@
 package com.example.albertdenova.threadingexercise.view.model;
 
 import android.app.Application;
+import android.app.NotificationManager;
 import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Transformations;
 import android.support.annotation.NonNull;
+import android.support.v4.app.NotificationCompat;
 
+import com.example.albertdenova.threadingexercise.ThreadingExerciseApplication;
 import com.example.albertdenova.threadingexercise.manager.ThreadCounterManager;
 import com.example.albertdenova.threadingexercise.model.ThreadCounter;
+
+import java.util.UUID;
 
 import javax.inject.Inject;
 
 import toothpick.Scope;
 import toothpick.Toothpick;
 
-
 public class ThreadCounterViewModel extends AndroidViewModel {
 
     @Inject ThreadCounterManager threadCounterManager;
+
+    @Inject NotificationManager notificationManager;
+
+    private int notificationId = 0;
+
 
     public ThreadCounterViewModel(@NonNull Application application) {
         super(application);
@@ -28,11 +38,31 @@ public class ThreadCounterViewModel extends AndroidViewModel {
     }
 
     public LiveData<ThreadCounter> getShortThreadCounter() {
-        return threadCounterManager.getThreadCounter(ThreadCounterManager.SHORT_TIME_MS);
+        return Transformations.map(
+                threadCounterManager.getThreadCounter(ThreadCounterManager.SHORT_TIME_MS),
+                threadCounter -> {
+                    if(threadCounter != null && threadCounter.getCount() % 10 == 0)
+                    {
+                        SetupCelebrationNotification(threadCounter.getCount());
+                    }
+                    return threadCounter;
+                });
     }
 
     public LiveData<ThreadCounter> getLongThreadCounter() {
         return threadCounterManager.getThreadCounter(ThreadCounterManager.LONG_TIME_MS);
+    }
+
+    public void SetupCelebrationNotification(Integer threadCount) {
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(getApplication(), ThreadingExerciseApplication.NOTIFICATION_CHANNEL_ID)
+                .setSmallIcon(android.support.v4.R.drawable.notification_icon_background)
+                .setContentTitle("Short thread celebration")
+                .setContentText(String.format("We have achieved %d number", threadCount))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        ++notificationId;
+
+        notificationManager.notify(notificationId, notificationBuilder.build());
     }
 
 }
